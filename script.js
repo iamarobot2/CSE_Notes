2c048275f01c5a595a59aa7da1c2acb2c8d4ef9b
Avin Joshy <avinjoshy21@gmail.com>
2023-10-09 21:06:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var user = 'iamarobot2';
 var repo = 'CSE_Notes';
-var yourToken = process.env.CSE_NOTES;
+var yourToken = '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';
 
 fetch('https://api.github.com/repos/iamarobot2/CSE_Notes/contents/notes')
     .then(response => response.json())
