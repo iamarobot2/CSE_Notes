35857e7d29d5d021e42fe1ae3c5a7f9c5a689596
Avin Joshy <avinjoshy21@gmail.com>
2023-10-09 18:52:41 +0530

    Add Main

---
+++ script.js
@@ -0,0 +1,30 @@
+
+var user = 'username';
+var repo = 'repository';
+
+fetch('https://api.github.com/repos/' + user + '/' + repo + '/contents')
+    .then(response => response.json())
+    .then(data => {
+        var cards = document.getElementById('cards');
+        data.forEach(item => {
+            if (item.name.endsWith('.pdf')) {
+                var card = document.createElement('div');
+                card.className = 'card col-md-4 p-3';
+                card.innerHTML = `
+                    <img src="pdf_icon.png" alt="PDF Icon" style="width:100%">
+                    <div class="container mt-3">
+                        <h4><b>${item.name}</b></h4> 
+                        <p>Description of the document.</p> 
+                        <a href="${item.download_url}" target="_blank" class="btn btn-primary">Open Document</a>
+                    </div>`;
+                cards.appendChild(card);
+            }
+        });
+    });
+
+document.getElementById('uploadForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var title = document.getElementById('title').value;
+    var file = document.getElementById('file').files[0];
+    // TODO: Upload file to GitHub
+});
