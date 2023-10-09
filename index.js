173adf8914b2319756290ad1a3f93af843eaefe9
Avin Joshy <avinjoshy21@gmail.com>
2023-10-10 01:49:03 +0530

    install express,cors

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const express = require('express');
 const app = express();
+
+app.get('/api/token', (req, res) => {
+    const token = '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';
+    res.json({ token: token });
+});
+
 app.listen(process.env.PORT || 3000);
