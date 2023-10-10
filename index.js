aee125e527d00c601fd7283ee957e36a1462eb2c
Avin Joshy <avinjoshy21@gmail.com>
2023-10-10 13:30:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 exports.handler = async (req, res) => {
     const file = req.files.file;
     const notesFolderPath = `${process.cwd()}/notes`;
-    await file.save(notesFolderPath);
-    res.status(200).send('File uploaded successfully!');
+  
+    try {
+      await file.save(notesFolderPath);
+      res.status(200).send('File uploaded successfully!');
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('An error occurred while uploading the file.');
+    }
   };
+  
   exports.handler = async (req, res) => {
     const filePath = req.query.filePath;
     await fs.unlink(filePath);
+
     res.status(200).send('File deleted successfully!');
   };
-  
   
